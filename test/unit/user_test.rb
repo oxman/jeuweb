f432f2a14cbe70e5b8c0d44de4ade619696820d1
Romain Tribes <tribes.romain@gmail.com>
2013-01-13 15:20:02 +0100

    Refactoring test.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -3,13 +3,13 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   def test_user_can_create_topic
     ability = Ability.new(FactoryGirl.create(:user))
-    assert ability.can?(:create, Topic)
+    assert ability.can?(:create, Topic), 'User should be able to create topic'
   end
 
 
   def test_nil_user_cant_create_topic
     ability = Ability.new(nil)
-    assert !ability.can?(:create, Topic)
+    assert !ability.can?(:create, Topic), 'Guest user should not be able to create topics'
   end
 
 
@@ -38,5 +38,12 @@ class UserTest < ActiveSupport::TestCase
     reply   = FactoryGirl.create(:reply, author: FactoryGirl.create(:user))
     ability = Ability.new(FactoryGirl.create(:user))
     assert !ability.can?(:update, reply), "User should not be able to edit other user's reply"
+  end
+
+
+  def test_nil_user_cant_edit_other_users_replies
+    reply   = FactoryGirl.create(:reply, author: FactoryGirl.create(:user))
+    ability = Ability.new(nil)
+    assert !ability.can?(:update, reply), "Guest user should not be able to edit any reply"
   end
 end
