da87aa972e632f816ca89793f8d1a637f0fdd16d
Romain Tribes <tribes.romain@gmail.com>
2013-01-20 19:03:39 +0100

    Refactoring.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -15,10 +15,9 @@ class Topic < ActiveRecord::Base
 
 
   def self.with_read_marks_for(user)
-    return Topic.scoped unless user
-    Topic
-      .select('topics.*, CASE WHEN read_marks.id IS NULL THEN 0 WHEN read_marks.reply_id < topics.last_reply_id THEN 0 ELSE 1 END AS read')
-      .joins('LEFT JOIN read_marks ON read_marks.topic_id = topics.id AND read_marks.user_id = %d' % [ user.id ])
+    return scoped unless user
+    select('topics.*, CASE WHEN read_marks.id IS NULL THEN 0 WHEN read_marks.reply_id < topics.last_reply_id THEN 0 ELSE 1 END AS read')
+    .joins('LEFT JOIN read_marks ON read_marks.topic_id = topics.id AND read_marks.user_id = %d' % [ user.id ])
   end
 
 
