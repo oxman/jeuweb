4da2045c6cae7d5a5d167d2dc857ac60f6e2ae4f
Romain Tribes <tribes.romain@gmail.com>
2013-01-20 18:34:37 +0100

    Add a flag to know if a reply has been scored by a user.

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -9,7 +9,19 @@ class Reply < ActiveRecord::Base
   paginates_per 50
 
 
+  def self.with_score_values_for(user)
+    return scoped unless user
+    select('replies.*, scores.value AS score_value')
+    .joins("LEFT JOIN scores ON scores.scorable_type = 'Reply' AND scores.scorable_id = replies.id AND scores.user_id = %d" % [ user.id ])
+  end
+
+
   def score
     scores.sum(:value)
+  end
+
+
+  def score_value
+    read_attribute(:score_value)
   end
 end
--- test/unit/reply_test.rb
+++ test/unit/reply_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class ReplyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_replies_have_score_for_user
+    user        = FactoryGirl.create(:user)
+    topic       = FactoryGirl.create(:topic)
+    first_reply = FactoryGirl.create(:reply, topic: topic)
+    last_reply  = FactoryGirl.create(:reply, topic: topic)
+    user.score(first_reply, Score::POSITIVE)
+
+    replies = topic.replies.with_score_values_for(user).order(:id)
+    assert_equal 1,   replies[0].score_value
+    assert_equal nil, replies[1].score_value
+  end
+
+
+  def test_replies_have_no_score_for_nil_user
+    topic = FactoryGirl.create(:topic)
+    FactoryGirl.create(:reply, topic: topic)
+
+    replies = topic.replies.with_score_values_for(nil)
+    assert_equal nil, replies[0].score_value
+  end
 end
