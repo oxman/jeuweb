349538b65eae1d7255deafb751db7477a891440d
Romain Tribes <tribes.romain@gmail.com>
2013-02-09 14:13:51 +0100

    Display only public topics in the topics pages.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,19 +1,19 @@
 class TopicsController < ApplicationController
   def index
-    @topics = Topic.with_read_marks_for(current_user).page(params[:page]).order('last_activity_at DESC').includes(:tags)
+    @topics = Topic.public.with_read_marks_for(current_user).page(params[:page]).order('last_activity_at DESC').includes(:tags)
   end
 
 
   def search
     @tags          = Tag.scoped
     @searched_tags = Tag.where(name: tag_names)
-    @topics        = Topic.with_read_marks_for(current_user).with_tags(@searched_tags).page(params[:page]).order('last_activity_at DESC').includes(:tags)
+    @topics        = Topic.public.with_read_marks_for(current_user).with_tags(@searched_tags).page(params[:page]).order('last_activity_at DESC').includes(:tags)
   end
 
 
   def new
     authorize! :create, Topic
-    @topic = Topic.new
+    @topic = Topic.public.new
   end
 
 
@@ -26,20 +26,20 @@ class TopicsController < ApplicationController
 
 
   def show
-    @topic   = Topic.with_score_values_for(current_user).find(params[:id])
+    @topic   = Topic.public.with_score_values_for(current_user).find(params[:id])
     @replies = @topic.replies.page(params[:page]).order('created_at ASC').with_score_values_for(current_user).includes(:author)
     current_user.read_topic(@topic, @replies.last) if current_user
   end
 
 
   def edit
-    @topic = Topic.find(params[:id])
+    @topic = Topic.public.find(params[:id])
     authorize! :update, @topic
   end
 
 
   def update
-    topic = Topic.find(params[:id])
+    topic = Topic.public.find(params[:id])
     authorize! :update, topic
     topic.update_attributes!(topic_params)
     topic.tag_with(tag_names)
@@ -48,7 +48,7 @@ class TopicsController < ApplicationController
 
 
   def tag
-    topic = Topic.find(params[:id])
+    topic = Topic.public.find(params[:id])
     authorize! :tag, topic
     topic.tag_with(tag_names)
     redirect_to(topic)
@@ -56,7 +56,7 @@ class TopicsController < ApplicationController
 
 
   def score
-    topic = Topic.find(params[:id])
+    topic = Topic.public.find(params[:id])
     authorize! :score, topic
     current_user.score(topic, score_value)
     redirect_to(:back)
